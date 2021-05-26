3229d48878c0845cd2d819073eda8e46ff26584f
DuchLovePiaa <81536678+DuchLovePiaa@users.noreply.github.com>
2021-05-26 17:58:47 +0700

    Add files via upload

---
+++ rove.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name            Rove
+// @version         0.122
+// @description     
+// @author          Author
+// @include         *
+// @grant           GM_xmlhttpRequest
+// @run-at          document-end
+// @downloadURL     
+// @updateURL       
+// ==/UserScript==
+
+/*  Obfuscator  //
+mangled 
+hexadec 
+//  Obfuscator  */
+
+var dllink                  = null;
+var domain                  = (window.location != window.parent.location) ? document.referrer.toString() : document.location.toString();
+var dolist                  = domain.replace("http://","").replace("https://","").replace("http://www.","").replace("https://www.","").replace("www.","").split("/")[0];
+
+if(
+    domain.match('all-access.wax.io/cloud-wallet/signing')
+){
+	(function thiscode(){
+		if(
+			document.querySelector('div[class*="react-ripples"]') && 
+			document.querySelector('div[class*="react-ripples"]').querySelector('button') && 
+			!document.querySelector('div[class*="react-ripples"]').querySelector('button').disabled
+		){
+			setInterval(function(){ document.querySelector('div[class*="react-ripples"]').querySelector('button').click() }, 1500);
+		}else{
+			setTimeout(function(){thiscode()}, 1000); 
+		}; 
+	})(); setInterval(function(){ document.querySelector('div[class*="react-ripples"]').parentElement.querySelectorAll('button')[1].click() }, 105000);
+}; 
