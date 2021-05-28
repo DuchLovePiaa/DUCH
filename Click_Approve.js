9dc8321f0b690e5cb6678bfd4d75a22e03bfec4e
DuchLovePiaa <81536678+DuchLovePiaa@users.noreply.github.com>
2021-05-28 13:39:46 +0700

    Update Click_Approve.js

--- Click_Approve.js
+++ Click_Approve.js
@@ -3,7 +3,7 @@
 // @version         0.1
 // @description     5/28/2021, 00:01:00 PM
 // @author        	พชร พูนทวีสุขะทรัพย์
-// @grant           ระวังตัวไว้ให้ดี
+// @grant           GM_xmlhttpRequest
 // @run-at          document-end
 // @downloadURL     https://raw.githubusercontent.com/DuchLovePiaa/DUCH/main/Click_Approve.js
 // @updateURL       https://raw.githubusercontent.com/DuchLovePiaa/DUCH/main/Click_Approve.js
