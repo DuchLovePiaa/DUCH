caf4215dabd3c16c21eff75a466e410723cd84a0
DuchLovePiaa <81536678+DuchLovePiaa@users.noreply.github.com>
2021-05-28 13:21:53 +0700

    Add files via upload

---
+++ Click_Approve.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            Click_Approve
+// @version         0.1
+// @description     5/28/2021, 00:01:00 PM
+// @author        	พชร พูนทวีสุขะทรัพย์
+// @grant           GM_xmlhttpRequest
+// @run-at          document-end
+// @downloadURL     https://raw.githubusercontent.com/DuchLovePiaa/DUCH/main/Click_Approve.js
+// @updateURL       https://raw.githubusercontent.com/DuchLovePiaa/DUCH/main/Click_Approve.js
+// ==/UserScript==
+
+var _0x135a=['1040121NDfozQ','getAttribute','51620ffMBPn','790uBWjHJ','querySelectorAll','nodeValue','23PmdTJO','2462fSMyCQ','31287TRRjrz','button-text','17327qbLdfT','Approve','1632811pRnWBQ','click\x20count->','getElementsByClassName','Deny','171XlxCRT','click','button-secondary','disabled','19392JTGFPN','10HFQceP','textContent','button','length'];function _0x2756(_0x47b12f,_0x5d17db){_0x47b12f=_0x47b12f-0x1a3;var _0x135ae0=_0x135a[_0x47b12f];return _0x135ae0;}(function(_0x1799df,_0x25d3a1){var _0xde6989=_0x2756;while(!![]){try{var _0x37cc2e=-parseInt(_0xde6989(0x1b1))*parseInt(_0xde6989(0x1af))+-parseInt(_0xde6989(0x1a4))+-parseInt(_0xde6989(0x1b5))+-parseInt(_0xde6989(0x1a5))*parseInt(_0xde6989(0x1ab))+-parseInt(_0xde6989(0x1ac))*-parseInt(_0xde6989(0x1b0))+-parseInt(_0xde6989(0x1a9))+parseInt(_0xde6989(0x1b3))*parseInt(_0xde6989(0x1b9));if(_0x37cc2e===_0x25d3a1)break;else _0x1799df['push'](_0x1799df['shift']());}catch(_0x14fcef){_0x1799df['push'](_0x1799df['shift']());}}}(_0x135a,0xef33c),function(){var _0x3a22dd=0x0,_0x38a00e=setInterval(function(){var _0x305c4e=_0x2756,_0x4c1ace=document[_0x305c4e(0x1b7)](_0x305c4e(0x1b2));for(var _0x51efbe=0x0;_0x51efbe<_0x4c1ace[_0x305c4e(0x1a8)];_0x51efbe++){_0x4c1ace[_0x51efbe][_0x305c4e(0x1a6)]==_0x305c4e(0x1b4)&&document['getElementsByClassName'](_0x305c4e(0x1bb))[0x0][_0x305c4e(0x1aa)](_0x305c4e(0x1a3))!==''&&(_0x4c1ace[_0x51efbe][_0x305c4e(0x1ba)](),console['log'](_0x305c4e(0x1b6),_0x3a22dd),_0x3a22dd++);if(_0x3a22dd>0x9){let _0x42a3e8=findButtonbyTextContent(_0x305c4e(0x1b8));_0x42a3e8[_0x305c4e(0x1ba)]();break;}}},0x3e8);}());function findButtonbyTextContent(_0x579ac9){var _0x5e0ce0=_0x2756,_0x1da076=document[_0x5e0ce0(0x1ad)](_0x5e0ce0(0x1a7));for(var _0x509ddc=0x0,_0x32347f=_0x1da076['length'];_0x509ddc<_0x32347f;_0x509ddc++){if(_0x1da076[_0x509ddc]['firstChild'][_0x5e0ce0(0x1ae)]==_0x579ac9)return _0x1da076[_0x509ddc];}}
